4f7385f5b8e1488b2266982b0fc426fe05d1c730
Vanessa Park <bohyunparkv@gmail.com>
2022-10-07 20:26:51 +0900

    Initial setup

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cart from './pages/Cart/Cart';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
